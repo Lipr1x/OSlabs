d00ad92d90f4b7aa82232da3fa77dd54f8c15125
Lipr1x <eli2281337@mail.ru>
2020-12-09 14:06:41 +0300

    fix

--- lab2_Ovsyannikov/src/SystemCore.java
+++ lab2_Ovsyannikov/src/SystemCore.java
@@ -21,12 +21,32 @@ public class SystemCore {
         while (!systemProcesses.isEmpty()) {
             System.out.println("");
             System.out.println(circle + " цикл запущен");
-            for (int i = 0; i < systemProcesses.size(); i++) {
-                SystemProcess systemProcess = systemProcesses.get(i);
-                systemProcess.load(timeOfProcess + systemProcess.getPriority() * differenceTime);
+            for (int j = 0; j < systemProcesses.size(); j++) {
+                SystemProcess systemProcess = systemProcesses.get(j);
+                int maxTimeOfProcess = timeOfProcess + systemProcess.getPriority() * differenceTime;
+
+                for (int i = 0; i < systemProcess.systemFlows.size(); i++) {
+                    SystemFlow systemFlow = systemProcess.systemFlows.get(i);
+                    if (systemFlow.getMaxTime() > maxTimeOfProcess) {
+                        systemFlow.cutMaxTime(maxTimeOfProcess);
+                        System.out.println("Обнаружено прерывание, выделенное время: " + maxTimeOfProcess + ". индекс потока " + systemFlow.getTid() + " процесса с индексом " + systemProcess.potokid + ". Оставшееся время: " + systemFlow.getMaxTime() + " Изначальное время: " + systemFlow.getYetTime());
+                        break;
+                    } else if(systemFlow.getMaxTime() == maxTimeOfProcess){
+                        systemFlow.launch(systemProcess.potokid);
+                        systemProcess.systemFlows.remove(systemFlow);
+                        break;
+                    }
+                    else  {
+                        maxTimeOfProcess -= systemFlow.getMaxTime();
+                        systemFlow.launch(systemProcess.potokid);
+                        systemProcess.systemFlows.remove(systemFlow);
+                        i--;
+                    }
+                }
+
                 if (systemProcess.getIsEmpty()) {
-                    systemProcesses.remove(i);
-                    i--;
+                    systemProcesses.remove(j);
+                    j--;
                 }
             }
             circle++;
--- lab2_Ovsyannikov/src/SystemProcess.java
+++ lab2_Ovsyannikov/src/SystemProcess.java
@@ -3,13 +3,12 @@ import java.util.Random;
 
 public class SystemProcess {
 
-    private ArrayList<SystemFlow> systemFlows = new ArrayList<>();
-    private int potokid;
+    public ArrayList<SystemFlow> systemFlows = new ArrayList<>();
+    public int potokid;
     private int priority;
     private Random random = new Random();
     private int maxSizeOfFlow = 5;
     private int maxTimeOfFlow = 80;
-    private boolean isEmpty = false;
 
     public SystemProcess(int potokid, int priority) {
         this.potokid = potokid;
@@ -28,35 +27,12 @@ public class SystemProcess {
         }
     }
 
-    public void load(int maxTimeOfProcess) {
-        for (int i = 0; i < systemFlows.size(); i++) {
-            SystemFlow systemFlow = systemFlows.get(i);
-            if (systemFlow.getMaxTime() > maxTimeOfProcess) {
-                systemFlow.cutMaxTime(maxTimeOfProcess);
-                System.out.println("Обнаружено прерывание, выделенное время: " + maxTimeOfProcess + ". индекс потока " + systemFlow.getTid() + " процесса с индексом " + potokid + ". Оставшееся время: " + systemFlow.getMaxTime() + " Изначальное время: " + systemFlow.getYetTime());
-                break;
-            } else if(systemFlow.getMaxTime() == maxTimeOfProcess){
-                systemFlow.launch(potokid);
-                systemFlows.remove(systemFlow);
-                break;
-            }
-            else  {
-                maxTimeOfProcess -= systemFlow.getMaxTime();
-                systemFlow.launch(potokid);
-                systemFlows.remove(systemFlow);
-                i--;
-            }
-        }
-        if (systemFlows.isEmpty()) {
-            isEmpty = true;
-        }
-    }
     public void setPriority(int priority) {
         this.priority = priority;
     }
     public int getPriority() {return priority;}
 
     public boolean getIsEmpty() {
-        return isEmpty;
+        return systemFlows.isEmpty();
     }
 }
